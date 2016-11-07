1158276c0ce0fe72039dc5b8facd9843463405dd
lamar west <westl@uci.edu>
2016-11-06 17:51:58 -0800

    added flexbox styling, added scrollbar bottom functionality, added username

--- server.js
+++ server.js
@@ -3,26 +3,32 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
-
+const Moniker = require('moniker');
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
-
 const server = express()
     .use((req, res) => res.sendFile(INDEX))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
 //Create the Message class that will exist on the server
-var Message = function(content) {
+var Message = function(content, userName) {
     this.content = content;
+    this.userName = userName;
     this.timeStamp = new Date().toDateString();
 };
 
 //Create Array that will hold messages on the server side
 var messageArray = [];
-//on connected, send all messages stored
+//on connected, send all messages stored and give the user a random username
 io.on('connection', (socket) => {
+    //pass each user the chat log
     socket.emit('connection', messageArray);
+
+    var userName = Moniker.choose();
+    //give each user a username
+    socket.emit('logged in', userName);
+
 
     socket.on('message sent', (message) => {
         messageArray.push(message);
@@ -31,6 +37,5 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         io.emit('user disconnected');
-        console.log('user disconnected');
     });
 });
